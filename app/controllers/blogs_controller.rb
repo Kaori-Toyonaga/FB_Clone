a4906cb1d6d6ec7e2f444ade7c2e1d3e1d11a4f7
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-04-05 18:11:51 +0000

    アソシエーション実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -9,7 +9,7 @@ class BlogsController < ApplicationController
   end
 
   def create
-    @blog = Blog.new(blog_params)
+    @blog = current_user.blogs.build(blog_params)
     if params[:back]
       render :new
     else
@@ -41,7 +41,7 @@ class BlogsController < ApplicationController
   end
 
   def confirm
-    @blog = Blog.new(blog_params)
+    @blog = current_user.blogs.build(blog_params)
     render :new if @blog.invalid?
   end
 
