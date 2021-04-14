0e732771664ef51d8ab146edb27af4f383c7e8fb
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-04-14 18:48:25 +0000

    プロフィール編集画面へのリダイレクト

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show] do
+    collection do
+      get :edit
+    end
+  end
   resources :blogs do
     collection do
       post :confirm
