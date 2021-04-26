9be342a5ddfbc9cc58ba528734382f863b09b233
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-04-26 21:00:35 +0000

    バリデーションコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 30 }, on: :create
-  validates :email, presence: true, length: { maximum: 50 }, on: :create
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, on: :create
-  validates :email, uniqueness: true, on: :create
+  validates :email, presence: true, length: { maximum: 50 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true, on: :create
   before_validation { email.downcase! }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, on: :create
