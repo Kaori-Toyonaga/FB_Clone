680cda9e5c6c6362cf76f73bf957d390b84f59d0
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-04-26 21:08:47 +0000

    ルート修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   resources :blogs do
     collection do
       post :confirm
-      # get '/top', to: 'blogs#top'
     end
   end
   resources :favorites, only: [:index, :create, :destroy]
