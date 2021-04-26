91a4e43bf4375bc9498aee0a518d422d049e561c
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-04-26 20:25:32 +0000

    ルーティン修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'blogs#top'
+  root to: 'users#new'
   resources :contacts
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
   resources :blogs do
     collection do
       post :confirm
-      # get '/top', to: 'blogs#top'
+      get '/top', to: 'blogs#top'
     end
   end
   resources :favorites, only: [:index, :create, :destroy]
