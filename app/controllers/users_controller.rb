a522d2c01569426a4036ce3edc98cf0f2faa5ec6
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-04-27 12:51:38 +0000

    recommit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,6 @@ class UsersController < ApplicationController
   def index
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
-
   def new
     @user = User.new
   end
@@ -21,6 +17,10 @@ class UsersController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def edit
